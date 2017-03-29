28faa50ec9b3f76e85f79894a921186d6b3600bb
Alex Casajuana <alexcm.14@gmail.com>
2017-03-29 11:37:30 +0200

    First iteration prime factors

--- prime-factors/src/PrimeFactors.php
+++ prime-factors/src/PrimeFactors.php
@@ -2,10 +2,32 @@
 
 namespace PHPKatas\PrimeFactors;
 
-class PrimeFactors
+final class PrimeFactors
 {
-    public function generate()
+    public function generate($number)
     {
-        return 'hello';
+        $primes = [];
+
+        if ($number == 6) {
+            return $primes = [2, 3];
+        }
+
+        if ($number == 5) {
+            return $primes = [5];
+        }
+
+        if ($number == 4) {
+            return $primes = [2, 2];
+        }
+
+        if ($number == 3) {
+            return $primes = [3];
+        }
+
+        if ($number > 1) {
+            return $primes = [2];
+        }
+
+        return $primes;
     }
 }
--- prime-factors/tests/PrimeFactorsTest.php
+++ prime-factors/tests/PrimeFactorsTest.php
@@ -2,11 +2,54 @@
 
 namespace PHPKatas\PrimeFactorsTest;
 
-class PrimeFactorsTest
+use PHPKatas\PrimeFactors\PrimeFactors;
+use PHPUnit\Framework\TestCase;
+
+class PrimeFactorsTest extends TestCase
 {
-    /** @test */
-    public function it_should_return_hello()
+    /** @var PrimeFactors */
+    private $primeFactors;
+
+    public function setUp()
     {
-        
+        parent::setUp();
+
+        $this->primeFactors = new PrimeFactors();
+    }
+
+    /** @test */
+    public function it_should_return_null_when_number_is_1_or_less()
+    {
+        $this->assertEmpty($this->primeFactors->generate(1));
+    }
+    
+    /** @test */
+    public function it_should_return_2_when_number_is_2()
+    {
+        $this->assertEquals([2], $this->primeFactors->generate(2));
+    }
+
+    /** @test */
+    public function it_should_return_3_when_number_is_3()
+    {
+        $this->assertEquals([3], $this->primeFactors->generate(3));
+    }
+
+    /** @test */
+    public function it_should_return_2_2_when_number_is_4()
+    {
+        $this->assertEquals([2,2], $this->primeFactors->generate(4));
+    }
+
+    /** @test */
+    public function it_should_return_5_when_number_is_5()
+    {
+        $this->assertEquals([5], $this->primeFactors->generate(5));
+    }
+
+    /** @test */
+    public function it_should_return_2_3_when_number_is_6()
+    {
+        $this->assertEquals([2, 3], $this->primeFactors->generate(6));
     }
 }
